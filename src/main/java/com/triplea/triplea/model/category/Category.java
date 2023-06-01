2f1d410cf7a27e68366dedd10e345830dbb98bf8
ju-ei8ht <qkr8125@gmail.com>
2023-06-01 12:46:27 +0900

    feat: add category, data #7

---
+++ src/main/java/com/triplea/triplea/model/category/Category.java
@@ -0,0 +1,30 @@
+package com.triplea.triplea.model.category;
+
+import com.triplea.triplea.model.bookmark.BookmarkCategory;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity @Getter
+@NoArgsConstructor
+@Table(name = "category_tb")
+public class Category {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String category;
+    private String mainCategory;
+
+    @OneToMany(mappedBy = "category", orphanRemoval = true)
+    private List<BookmarkCategory> bookmarkCategories = new ArrayList<>();
+
+    @Builder
+    public Category(Long id, String category, String mainCategory) {
+        this.id = id;
+        this.category = category;
+        this.mainCategory = mainCategory;
+    }
+}
