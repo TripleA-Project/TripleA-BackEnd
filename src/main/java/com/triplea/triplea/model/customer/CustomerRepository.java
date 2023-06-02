b133e66e096c0cb7b4da4b0f32cc0efc03c78cd2
ju-ei8ht <qkr8125@gmail.com>
2023-06-02 19:38:01 +0900

    feat: add customer #6

---
+++ src/main/java/com/triplea/triplea/model/customer/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.triplea.triplea.model.customer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    @Query("select c from Customer c where c.user.id=:id")
+    Optional<Customer> findCustomerByUserId(@Param("id") Long id);
+}
