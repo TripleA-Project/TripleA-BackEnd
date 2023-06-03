c4979c53be7c49ff5f80ae31f5a16fae39405d0d
ju-ei8ht <qkr8125@gmail.com>
2023-06-03 21:01:59 +0900

    feat: add translate method #20

---
+++ src/main/java/com/triplea/triplea/core/util/Translator.java
@@ -0,0 +1,43 @@
+package com.triplea.triplea.core.util;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.triplea.triplea.core.exception.Exception500;
+import com.triplea.triplea.dto.news.NewsRequest;
+import okhttp3.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.StringJoiner;
+
+@Component
+public class Translator {
+    private final OkHttpClient CLIENT = new OkHttpClient();
+    private final MediaType MEDIATYPE = MediaType.parse("application/json");
+    private final ObjectMapper OM = new ObjectMapper();
+    @Value("${translate.token}")
+    private String TOKEN;
+
+    public String translate(String content) throws IOException {
+        String[] contents = content.split("(?<=\\. )");
+        RequestBody requestBody = RequestBody.create(OM.writeValueAsString(NewsRequest.TranslateIn.builder().contents(contents).build()), MEDIATYPE);
+        Request request = new Request.Builder().url("https://wisetranslate.net/api/translate/ai")
+                .post(requestBody)
+                .headers(Headers.of("accept", MEDIATYPE.type(), "content-type", MEDIATYPE.type(), "Authorization", TOKEN))
+                .build();
+        try (Response response = CLIENT.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                String json = response.body() != null ? response.body().string() : "";
+                if (!json.isEmpty()) {
+                    StringJoiner result = new StringJoiner(" ");
+                    JsonNode rootNode = OM.readTree(json);
+                    for (JsonNode translation : rootNode.path("translation")) result.add(translation.asText());
+                    return result.toString().trim();
+                }
+                throw new Exception500("translate Response 실패");
+            }
+            throw new Exception500("translate 실패");
+        }
+    }
+}
