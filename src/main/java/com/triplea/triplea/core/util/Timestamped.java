8d04762cf5cbfc801e4933dc600cf67b0e69d32a
ju-ei8ht <qkr8125@gmail.com>
2023-06-06 13:17:58 +0900

    refact: inherit createdAt from CreatedTimestamped in Timestamped

--- src/main/java/com/triplea/triplea/core/util/Timestamped.java
+++ src/main/java/com/triplea/triplea/core/util/Timestamped.java
@@ -1,22 +1,12 @@
 package com.triplea.triplea.core.util;
 
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @Getter
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public abstract class Timestamped {
-    public static final ZoneId SEOUL_ZONE_ID = ZoneId.of("Asia/Seoul");
-    @CreatedDate
-    private ZonedDateTime createdAt;
+public abstract class Timestamped extends CreatedTimestamped{
     @LastModifiedDate
     private ZonedDateTime updatedAt;
 }
