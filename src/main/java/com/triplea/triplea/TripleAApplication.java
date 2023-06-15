22f3e26bd1d837947b58973be83ad3a10de664fc
wooram <rla7360@gmail.com>
2023-06-15 16:07:59 +0900

    import 정리

--- src/main/java/com/triplea/triplea/TripleAApplication.java
+++ src/main/java/com/triplea/triplea/TripleAApplication.java
@@ -1,12 +1,9 @@
 package com.triplea.triplea;
 
-import com.triplea.triplea.model.user.User;
-import com.triplea.triplea.model.user.UserRepository;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
 public class TripleAApplication {
