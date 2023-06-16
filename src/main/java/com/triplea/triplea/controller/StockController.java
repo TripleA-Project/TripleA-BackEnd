c5a058afbffcc89a6caba2ffdbbb70b08bde9af4
ju-ei8ht <qkr8125@gmail.com>
2023-06-16 16:15:20 +0900

    feat: add stock index(nasdaq, dowJones, sp500) #45

---
+++ src/main/java/com/triplea/triplea/controller/StockController.java
@@ -0,0 +1,23 @@
+package com.triplea.triplea.controller;
+
+import com.triplea.triplea.dto.ResponseDTO;
+import com.triplea.triplea.dto.stock.StockResponse;
+import com.triplea.triplea.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+@RequiredArgsConstructor
+public class StockController {
+    private final StockService stockService;
+
+    @GetMapping("/stocks")
+    public ResponseEntity<?> getStockIndex() {
+        StockResponse.Index index = stockService.getStockIndex();
+        return ResponseEntity.ok().body(new ResponseDTO<>(index));
+    }
+}
