7691891b7ce74b8950ab1f9e9caaa92d128e9efa
ju-ei8ht <qkr8125@gmail.com>
2023-06-16 16:15:20 +0900

    feat: add stock index(nasdaq, dowJones, sp500) #45

--- src/main/java/com/triplea/triplea/core/config/JpaAuditingConfig.java
+++ src/main/java/com/triplea/triplea/core/config/JpaAuditingConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.time.ZonedDateTime;
 import java.util.Optional;
 
 @Configuration
+@EnableScheduling
 @EnableJpaAuditing(dateTimeProviderRef = "zonedDateTimeProvider")
 public class JpaAuditingConfig {
 
