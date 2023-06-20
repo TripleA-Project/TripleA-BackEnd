73e7ea15ad905a7b5046247c67949207515f7b0d
ju-ei8ht <qkr8125@gmail.com>
2023-06-20 19:57:02 +0900

    fix: timestamped annotation

--- src/main/java/com/triplea/triplea/core/util/timestamp/Timestamped.java
+++ src/main/java/com/triplea/triplea/core/util/timestamp/Timestamped.java
@@ -2,10 +2,15 @@ package com.triplea.triplea.core.util.timestamp;
 
 import lombok.Getter;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.ZonedDateTime;
 
 @Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class Timestamped extends CreatedTimestamped{
     @LastModifiedDate
     private ZonedDateTime updatedAt;
