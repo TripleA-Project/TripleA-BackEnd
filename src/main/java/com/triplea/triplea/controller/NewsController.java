b7cbb99e01db74e74728ab0fc2c254043af0267e
ju-ei8ht <qkr8125@gmail.com>
2023-06-27 17:55:59 +0900

    fix: change swagger dependency for jwt #108

--- src/main/java/com/triplea/triplea/controller/NewsController.java
+++ src/main/java/com/triplea/triplea/controller/NewsController.java
@@ -6,6 +6,8 @@ import com.triplea.triplea.dto.news.NewsRequest;
 import com.triplea.triplea.dto.news.NewsResponse;
 import com.triplea.triplea.model.user.User;
 import com.triplea.triplea.service.NewsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Tag(name = "뉴스")
 @RequestMapping("/api")
 @RequiredArgsConstructor
 @RestController
@@ -22,6 +25,7 @@ public class NewsController {
 
     private final NewsService newsService;
 
+    @Operation(summary = "뉴스 조회(최신)")
     @GetMapping("/news/latest")
     public ResponseEntity<?> getGlobalNews(@AuthenticationPrincipal MyUserDetails myUserDetails, @RequestParam("size") int size, @RequestParam("page") Long page){
 
@@ -33,6 +37,7 @@ public class NewsController {
         return ResponseEntity.ok().body(responseDTO);
     }
 
+    @Operation(summary = "뉴스 조회(심볼)")
     @GetMapping("/news")
     public ResponseEntity<?> getSymbolNews(@RequestParam(value = "symbol") @Valid String symbol,
                                            @AuthenticationPrincipal MyUserDetails myUserDetails,
@@ -48,6 +53,7 @@ public class NewsController {
     }
 
     // 뉴스 조회(키워드)
+    @Operation(summary = "뉴스 조회(키워드)")
     @GetMapping("/news/keyword")
     public ResponseEntity<?> getNewsByKeyword(@RequestParam("keyword") String keyword, @RequestParam("size") int size, @RequestParam("page") Long page, @AuthenticationPrincipal MyUserDetails myUserDetails){
         User user = null;
@@ -57,6 +63,7 @@ public class NewsController {
     }
 
     // 뉴스 조회(카테고리)
+    @Operation(summary = "뉴스 조회(카테고리)")
     @GetMapping("/news/category/{id}")
     public ResponseEntity<?> getNewsByCategory(@PathVariable Long id, @RequestParam("size") int size, @RequestParam("page") Long page, @AuthenticationPrincipal MyUserDetails myUserDetails){
         User user = null;
@@ -66,6 +73,7 @@ public class NewsController {
     }
 
     // 뉴스 상세 조회
+    @Operation(summary = "뉴스 상세 조회")
     @GetMapping("/auth/news/{id}")
     public ResponseEntity<?> getNewsDetails(@PathVariable Long id, @AuthenticationPrincipal MyUserDetails myUserDetails){
         NewsResponse.Details details = newsService.getNewsDetails(id, myUserDetails.getUser());
@@ -73,6 +81,7 @@ public class NewsController {
     }
 
     // 히스토리 조회
+    @Operation(summary = "히스토리 조회")
     @GetMapping("/auth/history")
     public ResponseEntity<?> getHistory(@RequestParam("year") int year, @RequestParam("month") int month, @AuthenticationPrincipal MyUserDetails myUserDetails){
         List<NewsResponse.HistoryOut> histories = newsService.getHistory(year, month, myUserDetails.getUser());
@@ -80,6 +89,7 @@ public class NewsController {
     }
 
     // AI 뉴스 분석
+    @Operation(summary = "AI 뉴스 분석")
     @PostMapping("/auth/news/{id}/ai")
     public ResponseEntity<?> getAnalysisAI(@PathVariable Long id, @RequestBody NewsRequest.AI ai, Errors errors, @AuthenticationPrincipal MyUserDetails myUserDetails){
         NewsResponse.Analysis analysis = newsService.getAnalysisAI(id, ai, myUserDetails.getUser());
---
+++ src/main/java/com/triplea/triplea/core/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.triplea.triplea.core.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+//@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI().addSecurityItem(new SecurityRequirement()
+                .addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("Triple A")
+                        .description("API TEST")
+                        .version("1.0"));
+    }
+
+    private SecurityScheme createAPIKeyScheme(){
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+}
